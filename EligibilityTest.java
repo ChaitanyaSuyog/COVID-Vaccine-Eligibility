b306e8335ec49524f95cc2c0f618bd72cc680b0b
Chaitanya S <chaitanya.suyog@gmail.com>
2021-03-28 22:48:46 +0530

    Created a new method
    Created a new method to check if the username is empty or not.

--- EligibilityTest.java
+++ EligibilityTest.java
@@ -3,23 +3,40 @@ import java.util.Scanner;
 
 public class EligibilityTest {
 
-    public static void main(String[] args) throws InterruptedException {
+	static Scanner scan = new Scanner(System.in);
+
+	public static void main(String[] args) throws InterruptedException {
+
+		System.out.println("Please give your name: ");
+		String userName;
+
+		// This is an easy way to create the userName variable outside the while loop and simplify your code
+		while ((userName = scan.nextLine()).isEmpty()) {
+			System.out.println("Please enter a valid username: ");
+		}
+
+		checkEligibility(userName);
+    }
+
+    public static void checkEligibility(String userName) throws InterruptedException {
 
 		int covidCaseCount =  11_908_910;
 
-    	Scanner scan = new Scanner(System.in);
-		System.out.println("Please give your name: ");
-
-		String userName = scan.nextLine();
 		System.out.println("Dear " + userName + ", Welcome to the COVID Vaccine Eligibility Check (CVEC), " +
 				"the COVID cases in India are currently-" + covidCaseCount + ".");
 		System.out.println("Press the 'T' key to check your temperature. ");
-
-		//double tempMeasure = ThreadLocalRandom.current().nextDouble(96, 98.9);
 		String tempMeasure = String.format("%.1f",ThreadLocalRandom.current().nextDouble(96, 98.9));
 
 		if(scan.nextLine().equals("T")){
-			System.out.println(userName + ", your temperature " +
+			System.out.print("Measuring your temperature");
+			Thread.sleep(1000);
+			System.out.print(".");
+			Thread.sleep(1000);
+			System.out.print(".");
+			Thread.sleep(1000);
+			System.out.print(". ");
+			Thread.sleep(1000);
+			System.out.print(userName + ", your temperature " +
 					"is at " + tempMeasure + " degrees.");
 		}
 		else {
@@ -35,9 +52,9 @@ public class EligibilityTest {
 
 		int patientAge = ThreadLocalRandom.current().nextInt(45, 100);
 
-	    if (patientAge >= 45 && patientAge <= 100) {
+		if (patientAge >= 45 && patientAge <= 100) {
 
-            System.out.println("Since you are " + patientAge + ", you are eligible for vaccination.");
-        }
-    }
+			System.out.println("Since you are " + patientAge + ", you are eligible for vaccination.");
+		}
+	}
 }
